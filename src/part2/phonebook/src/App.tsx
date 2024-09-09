08dd93f17ccd89b549ddf73a7f71b97d9a352c0d
Alexander Porro <alex1020pl@gmail.com>
2024-09-08 19:22:33 -0500

    part2 exercise 2.11 (phonebook)

--- src/part2/phonebook/src/App.tsx
+++ src/part2/phonebook/src/App.tsx
@@ -1,20 +1,36 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
-import { PERSONS } from './constants/person.constants';
+import { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
 import { Person } from './types/person.types';
 import { Filter, PersonForm, PersonList } from './components';
 
+const BASE_PATH = 'http://localhost:3005/persons';
+
 export default function App() {
-  const [persons, setPersons] = useState<Array<Person>>(PERSONS);
-  const [filteredPersons, setFilteredPersons] =
-    useState<Array<Person>>(persons);
+  const [persons, setPersons] = useState<Array<Person>>();
+  const [filteredPersons, setFilteredPersons] = useState<Array<Person>>();
 
   const updatePersons = (newPerson: Person) => {
-    setPersons((prevPersons) => prevPersons.concat(newPerson));
+    setPersons((prevPersons) => prevPersons?.concat(newPerson));
   };
+
+  const fetchData = useCallback(async () => {
+    const response = await axios.get(BASE_PATH);
+    const data: Array<Person> = response.data;
+
+    setPersons(data);
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   useEffect(() => {
     setFilteredPersons(persons);
   }, [persons]);
+
+  if (!persons || !filteredPersons) {
+    return <h1 id='error'>No data.</h1>;
+  }
 
   return (
     <main>
